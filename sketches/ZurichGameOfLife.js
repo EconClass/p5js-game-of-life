6ad94fcbe1ff0482c7df5d5e37947cc882bf40dc
EconClass <38009767+EconClass@users.noreply.github.com>
2018-04-14 04:34:51 -0400

    Problem with cell creation.
    The arrays created have objects inside them that correspond to the properties coded into the cell class, but the display shows no cells being created.

--- sketches/ZurichGameOfLife.js
+++ sketches/ZurichGameOfLife.js
@@ -12,39 +12,56 @@ function draw () {
 
 class Grid {
   constructor (cellSize) {
-  	this.cellSize = cellSize;
-  	this.numberOfRows = width / this.cellSize;
-  	this.numberOfColumns = height / this.cellSize;
-  	var x = this.numberOfRows; 
-  	var y = this.numberOfColumns; 
-  	var cells = new Array(x); 
-  	this.cells = cells;
-  	for (var i = 0; i < cells.length; i ++) { 
-  		cells[i] = new Array(y); 
-  	}
-  	for (var column = 0; column < this.numberOfColumns; column ++) { 
-  		for (var row = 0; row < this.numberOfRows; row++) {  
-  			this.cells[column][row] = new Cell(column, row, this.cellSize); 
-  		}
-  	}
-  	print (this.cells);
+    this.cellSize = cellSize;
+    this.numberOfRows = width / this.cellSize;
+    this.numberOfColumns = height / this.cellSize;
+    var x = this.numberOfRows; 
+    var y = this.numberOfColumns; 
+    var cells = new Array(x); 
+    this.cells = cells;
+    for (var i = 0; i < cells.length; i ++) { 
+      cells[i] = new Array(y); 
+    }
+    for (var column = 0; column < this.numberOfColumns; column ++) { 
+      for (var row = 0; row < this.numberOfRows; row++) {  
+        this.cells[column][row] = new Cell(column, row, this.cellSize);
+      }
+    }
+    print (this.cells);
   }
   draw () {
     for (var column = 0; column < this.numberOfColumns; column ++) {
       for (var row = 0; row < this.numberOfRows; row++) {
-        fill(240);
-        noStroke(); 
-        rect(column * this.cellSize + 1, row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
+        this.cells.drawCell;
       }
     }
   }
 }
 
 class Cell {
-	constructor (column, row, cellSize) {
-		this.column = column;
-		this.row = row;
-		this.cellSize = cellSize;
-		this.IsAlive = false;
-	}
+  constructor (column, row, cellSize) {
+    this.column = column;
+    this.row = row;
+    this.cellSize = cellSize;
+    this.IsAlive =  true; //setIsAlive(floor(random(1)));
+  }
+
+  drawCell () {
+    if (this.IsAlive = false) {
+      fill(240);
+    } else {
+      fill(0);
+    }
+    noStroke(); 
+    rect(this.column * this.cellSize + 1, this.row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
+  }
+
+  // setIsAlive (value) {
+  //   this.value = value;
+  //   if (this.value == 1){
+  //     this.IsAlive = true;
+  //   } else {
+  //     this.IsAlive = false;
+  //   }
+  // }
 }
