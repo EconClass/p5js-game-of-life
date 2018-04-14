99784d5f52ee145a5100a7f6cc8d2150f6c1cda9
EconClass <38009767+EconClass@users.noreply.github.com>
2018-04-14 17:26:48 -0400

    Add Cell count

--- sketches/ZurichGameOfLife.js
+++ sketches/ZurichGameOfLife.js
@@ -1,13 +1,17 @@
 var grid;
+//var IsAlive;
 
 function setup () {
   createCanvas(400, 400);
   grid = new Grid(20);
+  //IsAlive = Cell.setIsAlive(floor(random(2)));
 }
 
 function draw () {
   background(250);
+  //grid.updateNeighborCounts();
   grid.draw();
+  grid.randomize();
 }
 
 class Grid {
@@ -27,15 +31,40 @@ class Grid {
         this.cells[column][row] = new Cell(column, row, this.cellSize);
       }
     }
-    print (this.cells);
   }
+
   draw () {
     for (var column = 0; column < this.numberOfColumns; column ++) {
       for (var row = 0; row < this.numberOfRows; row++) {
-        this.cells.drawCell;
+        this.cells[column][row].drawCell();
       }
     }
   }
+
+  randomize () {
+    for (var column = 0; column < this.numberOfColumns; column ++) {
+      for (var row = 0; row < this.numberOfRows; row++) {
+        this.cells[column][row].setIsAlive(floor(random(2)));
+      }
+    }
+  }
+
+  // updateNeighborCounts () {
+  //   for (var column = 0; column < this.numberOfColumns; column ++) {
+  //     for (var row = 0; row < this.numberOfRows; row++) {
+  //       this.cells[column][row].liveNeighborCount();
+  //     }
+  //   }
+
+  //   for (var xOffset = -1; xOffset <= 1; xOffset++) {
+  //     for (var yOffset = -1; yOffset <= 1; yOffset++) {
+  //       var neighborX = currentCell.column + xOffset
+  //       var neighborY = currentCell.row + yOffset
+
+  //       // do something with neighborX and neighborY
+  //     }
+  //   }
+  // }
 }
 
 class Cell {
@@ -43,25 +72,29 @@ class Cell {
     this.column = column;
     this.row = row;
     this.cellSize = cellSize;
-    this.IsAlive =  true; //setIsAlive(floor(random(1)));
+    //this.cellNeighborCount = 0;
   }
 
   drawCell () {
-    if (this.IsAlive = false) {
-      fill(240);
+    if (this.IsAlive == false) {
+      fill(26, 44, 87);
     } else {
-      fill(0);
+      fill(209, 166, 131);
     }
     noStroke(); 
     rect(this.column * this.cellSize + 1, this.row * this.cellSize + 1, this.cellSize - 1, this.cellSize - 1);
   }
 
-  // setIsAlive (value) {
-  //   this.value = value;
-  //   if (this.value == 1){
-  //     this.IsAlive = true;
-  //   } else {
-  //     this.IsAlive = false;
-  //   }
+  setIsAlive (value) {
+    this.value = value;
+    if (this.value == 1){
+      this.IsAlive = true;
+    } else {
+      this.IsAlive = false;
+    }
+  }
+
+  // liveNeighborCount (){
+  //   this.cellNeighborCount = 0;
   // }
 }
